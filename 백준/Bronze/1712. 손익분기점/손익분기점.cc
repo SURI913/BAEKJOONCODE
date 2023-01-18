f119b82451129ac8153f89f6d8a6b66e5d7afbc8
SURI <101981952+SURI913@users.noreply.github.com>
2023-01-18 23:05:16 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int fc, vc, sc;
+	cin >> fc >> vc >> sc;
+
+	if (vc < sc) {
+		int amount = round(fc / (sc - vc) + 1);
+		cout << amount;
+	}
+	else if (vc == sc || vc > sc) {
+		cout << -1;
+	}
+	return 0;
+}
