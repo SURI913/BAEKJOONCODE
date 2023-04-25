756f91f5e8c8c11f9e7860d986ca71a31acfba21
SURI <101981952+SURI913@users.noreply.github.com>
2023-04-25 22:39:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair <int, int> xy1, pair<int, int> xy2) {	//compare은 bool값 반환
+	if (xy1.first == xy2.first) {	//x좌표가 같다면
+		return xy1.second < xy2.second;		//y좌표를 오름순으로
+	}
+	return xy1.first < xy2.first;	//x좌표가 같지 않다면 x좌표를 오름차순으로
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	//벡터 2차원으로 입력
+	vector <pair <int, int> > XY;	//pair로 2차원 선언
+	for (int i = 0; i < N; i++) {
+		int temp1, temp2;
+		cin >> temp1 >> temp2;
+		XY.push_back(make_pair(temp1, temp2)); //temp1 = frist, tmep2 = second
+	}
+
+	sort(XY.begin(), XY.end(), compare);
+
+	for (auto& result : XY) {
+		cout << result.first << " " << result.second << '\n';
+	}
+
+	return 0;
+}
