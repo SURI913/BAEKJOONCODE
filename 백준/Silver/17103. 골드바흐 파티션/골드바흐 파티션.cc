31fd7f0b972639b5e761c1832f7d27fb8924ca0a
이수경 <101981952+SURI913@users.noreply.github.com>
2023-09-02 17:51:55 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 308 ms, Memory: 9704 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int n;
+		cin >> n;
+		//에라토스테네스의 체 로 소수판별
+		vector <int> array(n + 1, 0); //소수 이다 아니다 판별
+		for (int i = 2; i <= n; i++) {
+			array[i] = i; //인덱스 값에 맞춰 수 입력
+		}
+
+		for (int i = 2; i * i <= n; i++) {
+			if (array[i] == 0) continue; //수가 지워진 경우 넘어감
+			for (int j = 2 * i; j <= n; j += i) {
+				if (array[j] == 0) continue; //소수 x라면 넘어감
+				else array[j] = 0; //소수 지움
+			}
+		}
+		//소수판별 끝
+		int count = 0;
+		for (int j = n / 2; j > 0; j--) {
+			if (array[j] && array[n - j]) {
+				count++;
+			}
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
