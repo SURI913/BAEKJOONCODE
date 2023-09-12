3c0412c7ee55d3f1447f9a6352d8e21bc36bb168
이수경 <101981952+SURI913@users.noreply.github.com>
2023-09-12 14:22:42 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+
+	int x[3], y[3];
+	for (int i = 0; i < 3; i++) {
+		cin >> x[i] >> y[i];
+	}
+
+	if (x[0] == x[1]) {
+		cout << x[2] << " ";
+	}
+	else if (x[0] == x[2]) {
+		cout << x[1] << " ";
+	}
+	else {
+		cout << x[0] << " ";
+	}
+
+	if (y[0] == y[1]) {
+		cout << y[2];
+	}
+	else if (y[0] == y[2]) {
+		cout << y[1];
+	}
+	else {
+		cout << y[0];
+	}
+	
+	return 0;
+}
