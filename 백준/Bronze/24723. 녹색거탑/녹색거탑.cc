0f2e9d1db9f1b533dd54a7a4289bcc1e9b0f49de
이수경 <101981952+SURI913@users.noreply.github.com>
2024-08-30 08:46:32 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int input;
+	cin >> input;
+	cout << pow(2, input);
+	return 0;
+
+}
